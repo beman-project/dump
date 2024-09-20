fee3152ad375b790e45a530741c8549dbc6084c4
Jonathan Müller <git@foonathan.net>
2024-09-19 21:53:47 -0600

    Add initial implementation of `std::dump`

---
+++ include/beman/dump/dump.hpp
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef BEMAN_DUMP_DUMP_HPP_INCLUDED
+#define BEMAN_DUMP_DUMP_HPP_INCLUDED
+
+#include <array>
+#include <print>
+#include <string_view>
+
+namespace beman::dump {
+    namespace detail {
+        template <std::size_t N>
+        constexpr auto format_string = []{
+            std::array<char, N * 3 - 1> result{};
+
+            auto ptr = &result[0];
+            for (auto i = 0uz; i != N; ++i)
+            {
+                if (i > 0)
+                    *ptr++ = ' ';
+                *ptr++ = '{';
+                *ptr++ = '}';
+            }
+
+            return result;
+        }();
+
+        template <>
+        inline constexpr std::array<char, 0> format_string<0>;
+    }
+
+    template <typename ... Args>
+    void dump(Args&&... args)
+    {
+        std::println(std::string_view(detail::format_string<sizeof...(Args)>), std::forward<Args>(args)...);
+    }
+}
+
+#endif // BEMAN_DUMP_DUMP_HPP_INCLUDED
+
--- src/beman/dump/dump.cpp
+++ src/beman/dump/dump.cpp
@@ -1,3 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include <beman/example/identity.hpp>
+#include <beman/dump/dump.hpp>
+
